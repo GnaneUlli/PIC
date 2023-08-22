6ce37b373246989c341e45a25ea9db6176851427
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-08-22 10:28:01 +0530

    Update _Ccl_assignment.c

--- _Ccl_assignment.c
+++ _Ccl_assignment.c
@@ -21,3 +21,32 @@
 unsigned char toggleFlag = 0; // Flag to track toggle state
 
 
+void main(){
+    TRISA0 = 0; // Set RA0 (pin 17) as output
+    RA0 = 0;    // Initialize the pin low
+    
+    // Configure Timer0
+    OPTION_REG = 0x04; // Prescaler 1:32, internal instruction cycle clock (Fosc/4)
+    TMR0 = 100;      // Initialize Timer0 value for a 5ms delay (256 - (5ms / (1 / (4MHz / 4 / 256))))
+    TMR0IE = 1;   // Enable Timer0 overflow interrupt
+    while (1) {
+         if (TMR0IF) {
+             TMR0IF = 0; // Clear Timer0 interrupt flag
+             
+             // Toggle the pin based on the toggleFlag
+             if (toggleFlag) {
+                 // Turn the pin HIGH for 15ms
+                 RA0 = 1;
+                 __delay_ms(150);
+                 // Turn the pin LOW for 10ms
+                 RA0 = 0;
+                 __delay_ms(100);
+                 toggleFlag = 0; // Set the flag for the next cycle
+             } else {
+                 toggleFlag = 1; // Set the flag for the next cycle
+             }
+             // Reload Timer0 for a 5ms delay (assuming 4 MHz clock)
+             TMR0 = 100; // 256 - (5ms / (1 / (4MHz / 4 / 256)))
+         }
+    }
+}
